530a7771085b85d98d72f8bd740eadba04693c94
leolaf78 <leo.lafornara@edu.ece.fr>
2024-01-31 11:57:15 +0100

    etape 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,47 @@
 #include <Arduino.h>
 #include <DHT.h>
 
-// Define the pins that we will use
 #define SENSOR 33
 #define LED 26
-#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT11   // Utilisation du DHT11
 
-// Initialize DHT sensor.
 DHT dht(SENSOR, DHTTYPE);
 
 void setup() {
-  Serial.begin(9600); // Start the serial communication
-  pinMode(LED, OUTPUT); // Initialize the LED pin as an output
-  dht.begin(); // Initialize the DHT sensor
-}
+  // Initialisation de la communication série
+  Serial.begin(9600);
+  dht.begin(); // Initialisation du capteur DHT
 
-void loop() {
-  // Effectuer une mesure toutes les 5s
-  delay(2000); // Wait for 5 seconds between measurements
+  // Laissez le temps à la communication série de s'initialiser
+  delay(1000); // Attendre 1 seconde
 
-  // Read humidity and temperature values
+  // Prendre une mesure
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
 
-  // Check if any reads failed and exit early (to try again).
+  // Vérification des lectures échouées
   if (isnan(humidity) || isnan(temperature)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+    // Affichage des valeurs de température et d'humidité
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" °C");
   }
 
-  // Afficher l'humidité relative avec un peu de formatage
-  Serial.print(F("Humidity: "));
-  Serial.print(humidity);
-  Serial.println(F("%"));
+  // Configuration du timer pour le Deep Sleep (5 secondes = 5000000 microsecondes)
+  esp_sleep_enable_timer_wakeup(5000000);
 
-  // Afficher la température avec un peu de formatage
-  Serial.print(F("Temperature: "));
-  Serial.print(temperature);
-  Serial.println(F("°C"));
+  delay(10);
+  Serial.flush();
+  // Mise en Deep Sleep
+  esp_deep_sleep_start();
+
+}
+
+void loop() {
+  // La fonction loop est vide car toute la logique est dans setup et l'ESP32 va dormir après chaque mesure
 }
